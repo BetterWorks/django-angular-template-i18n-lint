297e73efb73dd34d3dd784f8133ce5cb1dcd4155
Jacek <jacek@ivolution.pl>
2015-02-19 17:51:41 +0100

    missed file

--- django_template_i18n_lint.py
+++ django_template_i18n_lint.py
@@ -64,7 +64,7 @@ GOOD_STRINGS = re.compile(
         |(?:['"]\W+)?(?:data-|data-original-)?(?:alt|value|title|summary)=['"]?
 
          # Boolean attributes
-        |<[^<>]+?(?:checked|selected|disabled|readonly|multiple|ismap|defer|async|declare|noresize|nowrap|noshade|compact|hidden|itemscope|autofocus|autoplay|controls|download|novalidate)[^<>]*?>
+        |<[^<>]+?(?:checked|selected|disabled|readonly|multiple|ismap|defer|async|declare|noresize|nowrap|noshade|compact|hidden|itemscope|autofocus|autoplay|controls|download|novalidate|required)[^<>]*?>
 
          # Angular translated text
          # <div translate>value</div>
